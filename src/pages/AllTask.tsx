5ea7fed4b079e63f6f87a5cd159c942751d8027c
Tuncay Ekmez <tuncayekmez@gmail.com>
2024-05-24 18:19:17 +0300

    feat: Add filter functionality to AllTask page
    The code changes in this commit add filter functionality to the AllTask page. Users can now filter tasks based on their status (waiting, completed, canceled). This improves the user experience by allowing them to easily view tasks based on their status.

--- src/pages/AllTask.tsx
+++ src/pages/AllTask.tsx
@@ -1,9 +1,30 @@
 import { useAppSelector } from "../redux/hooks";
 import { RootState } from "../redux/store";
 import PageTemplate from "../components/PageTemplate";
+import { Chip, Group } from "@mantine/core";
+import { useState } from "react";
 const AllTask = () => {
+  const [value, setValue] = useState<string[]>(["react"]);
   const tasks = useAppSelector((state: RootState) => state.tasks.tasks);
-  return <PageTemplate tasks={tasks} />;
+  const filteredTasks = tasks.filter((task) => value.includes(task.status));
+  return (
+    <>
+      <Chip.Group multiple value={value} onChange={setValue}>
+        <Group justify="center" mb={12}>
+          <Chip color="green" value="waiting">
+            Waiting
+          </Chip>
+          <Chip color="green" value="completed">
+            Completed
+          </Chip>
+          <Chip color="green" value="canceled">
+            Canceled
+          </Chip>
+        </Group>
+      </Chip.Group>
+      <PageTemplate tasks={filteredTasks.length > 0 ? filteredTasks : tasks} />;
+    </>
+  );
 };
 
 export default AllTask;
